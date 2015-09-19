4793d6512a5acdc36c14b6bf1a5c0533d8483b42
Arnelle Balane <arnellebalane@gmail.com>
2015-09-19 16:30:10 +0800

    followed consistent coding style

--- js/main.js
+++ js/main.js
@@ -9,36 +9,40 @@ var initialB = 155;
 $(document).ready(function() {
     initSliders();
     initCanvas();
-    fillCanvas(initialR,initialG,initialB);
-    setValues(initialR,initialG,initialB);
+    fillCanvas(initialR, initialG, initialB);
+    setValues(initialR, initialG, initialB);
 });
 
+
 function initCanvas() {
-	canvas = document.getElementById("canvas");  
-	ctx = canvas.getContext("2d");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
+
 
 function fillCanvas(red, green, blue) {
-	ctx.fillStyle = "rgb("+red+","+green+","+blue+")";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
 
 function initSliders() {
-	$("input[type=range][orient=vertical]").on('input', function(e){
-        var red = $("#red").val();
-        var green = $("#green").val();
-        var blue = $("#blue").val();
+    $('input[type=range][orient=vertical]').on('input', function(e) {
+        var red = $('#red').val();
+        var green = $('#green').val();
+        var blue = $('#blue').val();
         fillCanvas(red, green, blue);
         setValues(red, green, blue);
- 	});
+    });
 }
 
+
 function setValues(red, green, blue) {
-	var hex = rgbToHex(red,green,blue);
-	$("#rgb").text("R: "+ red + " G: " + green + " B: " + blue);
-	$("#hex").text("HEX: "+ hex);
-	var complementColor = invertColor(hex);
-	$(".value").css('color', complementColor);
+    var hex = rgbToHex(red, green, blue);
+    var complementColor = invertColor(hex);
+    $('#rgb').text('R: ' + red + ' G: ' + green + ' B: ' + blue);
+    $('#hex').text('HEX: '+ hex);
+    $('.value').css('color', complementColor);
 }
